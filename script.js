ba2f6f606926f8f199ca6ea8bc3dfcd1adaa0bfb
dgleyramos1 <dgleysilva@gmail.com>
2021-10-28 17:48:35 -0300

    cronometro adicionado

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var altura= 0
 var largura = 0
 var vidas = 1
+var tempo = 10
 
 
 function ajustaTamanhoPalcoJogo(){
@@ -10,6 +11,16 @@ function ajustaTamanhoPalcoJogo(){
 
 ajustaTamanhoPalcoJogo()
 
+var cronometro = setInterval(function(){
+    tempo -= 1
+
+    if(tempo < 0){
+        clearInterval(cronometro)
+        clearInterval(criarmosquito)
+    }else {
+        document.getElementById("cronometro").innerHTML = tempo
+    }
+}, 1000)
 
 function posicaoRandomica() {
 
